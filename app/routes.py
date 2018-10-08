043ae685bf2f7df56b269da7fccd3abb0275f851
Christopher Byrd <cbyrd01@users.noreply.github.com>
2018-10-08 15:16:23 -0500

    Update site favicon

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,15 @@ from app import app
 
 def index():
     return render_template('index.html', title='IFMS Sign')
+
+@app.route('/favicon.ico')
+def favicon():
+    return app.send_static_file('favicon.ico')
+
+@app.route('/favicon-16x16.png')
+def favicon16():
+    return app.send_static_file('favicon-16x16.png')
+
+@app.route('/favicon-32x32.png')
+def favicon32():
+    return app.send_static_file('favicon-32x32.png')
