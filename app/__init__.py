59874ca41f0d97832a1c563c102b386ea1c94f11
Christopher Byrd <cbyrd01@users.noreply.github.com>
2018-10-09 23:04:30 -0500

    Added a colorpicker. :)

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_redislite import FlaskRedis
 from flask_bootstrap import Bootstrap
+from flask_colorpicker import colorpicker
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 rdb = FlaskRedis(app, collections = True)
 Bootstrap(app)
+colorpicker(app)
 
 from app import routes
